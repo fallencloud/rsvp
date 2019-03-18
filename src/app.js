a4dec4bdeade67bba749c97fac53530046e7c027
Sharina V. Jones <sharinavjones@gmail.com>
2019-03-18 09:26:21 -0400

    adding and simple workflow

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 class InviteeClass {
   constructor() {
+    //creates invitees
     this.invitees = [
       {name: 'Sharina Jones', isConfirmed: false},
       {name: 'Anthony Ibarra', isConfirmed: false},
@@ -10,28 +11,33 @@ class InviteeClass {
   this.addEventListeners();
   }//end constructor()
 
+  //loads invitees
   loadInvitees() {
     let inviteeHtml = this.invitees.reduce((html, invitee, index) => html += this.generateInviteeHtml(invitee, index), '');
     document.getElementById('invitedList').innerHTML = inviteeHtml;
   }//end loadInvitees()
 
+  //creates a card for every invitee
   generateInviteeHtml(invitee, index) {
     return `
     <li>${invitee.name}<label>Confirmed <input type="checkbox" ${invitee.isConfirmed ? 'checked' : ''}onChange="invitee.toggleInviteeStatus(${index})"></label><button onClick="invitee.deleteInvitee(event, ${index})">remove</button></li>
     `
   }//end generateInviteeHtml
 
+  //allows the status of invitees to be chagned
   toggleInviteeStatus(index) {
     this.invitees[index].isConfirmed = !this.invitees[index].isConfirmed;
     this.loadInvitees();
   }//end toggleInviteeStatus()
 
+  //allows an invitee to be deleted
   deleteInvitee(event, inviteeIndex) {
     event.preventDefault();
     this.invitees.splice(inviteeIndex, 1);
     this.loadInvitees();
   }//end deleteInvitee()
 
+  //ensures that enter and submit work
   addEventListeners() {
     document.getElementById('registrar').addEventListener('submit', (event) => {
       event.preventDefault();
@@ -42,7 +48,7 @@ class InviteeClass {
       this.addInvitee(invitee);
     });
   }
-
+  //allows an invitee to be added
   addInvitee(invitee) {
     let newInvitee = {
       name: invitee,
